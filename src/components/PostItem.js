9905df8cee2ef584d5300c4160d965ec629276ec
Vinicius <viniciusflores379@gmail.com>
2020-03-29 16:47:16 -0300

    :zap: feito exercicio

---
+++ src/components/PostItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function PostItem({ author, date, content, comments }) {
+  return ( 
+    <div className="post">
+      <div className="post-header">
+        <img className="avatar" src={author.avatar} />
+        <div className="details">
+          <span>{author.name}</span>
+          <span>{date}</span>
+        </div>
+      </div>
+      <p className="post-content">{content}</p>
+      <div className="post-comments">
+      <div className="divider" />
+        {comments.map(comment => (
+          <div key={comment.id} className="comment">
+            <img className="avatar" src={comment.author.avatar} />
+            <p>
+              <span>{comment.author.name}</span>
+              {comment.content}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+    
+  )
+}
+
+export default PostItem
